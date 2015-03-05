02a647148fa7c8a7727cd11d9879c2be453d5356
thom <thom.rs7@gmail.com>
2015-03-05 18:59:59 -0500

    Formatting

--- plot4.R
+++ plot4.R
@@ -19,15 +19,15 @@ filtered_data$timestamp <- as.POSIXct(filtered_data$timestamp, format="%m/%d/%Y 
 
 png(filename = "plot4.png", width = 480, height = 480)
 par(mfrow=c(2,2))
-# Upper Left Corner
+
 plot(filtered_data$timestamp, filtered_data$Global_active_power, type = "l", xlab = "", ylab= "Global Active Power (kilowatts)")
-# Upper Right Corner
+
 plot(filtered_data$timestamp, filtered_data$Voltage, type = "l", xlab = "", ylab= "Voltage", main = "")
-# Lower Left Corner
+
 plot(filtered_data$timestamp, filtered_data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering", main = "")
 lines(filtered_data$timestamp, filtered_data$Sub_metering_2, col = "red")
 lines(filtered_data$timestamp, filtered_data$Sub_metering_3, col = "blue")
 legend("topright", lty = 1, bty = "n", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("black","red","blue"))
-# Lower Right Corner
+
 plot(filtered_data$timestamp, filtered_data$Global_reactive_power, type = "l", xlab = "", ylab= "Global_rective_power")
 dev.off()
