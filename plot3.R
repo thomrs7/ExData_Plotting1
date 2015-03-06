4ede5fec13445f9742f0d274ed22a3cb581393f3
thom <thom.rs7@gmail.com>
2015-03-05 19:01:09 -0500

    Clean up

--- plot3.R
+++ plot3.R
@@ -1,4 +1,3 @@
-setwd('~/R/ExData_Plotting1')
 library(data.table)
 library(dplyr)
 
@@ -11,12 +10,9 @@ data$D <- as.Date(data$Date, "%d/%m/%Y")
 
 filtered_data <- filter(data, data$D == '2007-02-01' | data$D == '2007-02-02')
 
-
 filtered_data <- within(filtered_data, { timestamp=format(as.POSIXct(paste(D, Time)), "%m/%d/%Y %H:%M:%S") })
 
 filtered_data$timestamp <- as.POSIXct(filtered_data$timestamp, format="%m/%d/%Y %H:%M:%S")
-
-
 
 png(filename = "plot3.png", width = 480, height = 480)
 plot(filtered_data$timestamp, filtered_data$Sub_metering_1, col='black', type='l',ylab = "Global Active Power (kilowatts)", xlab = "")
